030a39919cb9bbde7a9638f0f21bee51f13e4a2d
Muhammad Fadhlan Karimuddin <138226358+poemich@users.noreply.github.com>
2024-09-18 15:13:50 +0700

    tutorial 3 commit 0

--- main/models.py
+++ main/models.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 import uuid
 
 # Create your models here.
 class MoodEntry(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # T3
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
     feelings = models.TextField()
